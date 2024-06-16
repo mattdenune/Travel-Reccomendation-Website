82b0bee5000d0bb12775dedcb08edf202f0af9f6
mattdenune <mattdenune@gmail.com>
2024-06-16 15:48:52 -0400

    project completed

--- travel_reccommendation.js
+++ travel_reccommendation.js
@@ -1,38 +1,63 @@
 const btnSearch = document.getElementById("btnSearch");
+const btnReset = document.getElementById("btnReset");
+const searchInput = document.getElementById("conditionDestination").value;
 
+function searchLocation() {
+  const input = document
+    .getElementById("conditionDestination")
+    .value.toLowerCase();
+  console.log("Input:", input);
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
 
-function searchCondition() {
-    const input = document.getElementById("conditionDestination").value.toLowerCase();
-    const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = "";
-  
-    fetch("travel_reccommendation_api.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const condition = data.conditions.find(
-          (item) => item.name.toLowerCase() === input
-        );
-  
-        if (condition) {
-          const symptoms = condition.symptoms.join(", ");
-          const prevention = condition.prevention.join(", ");
-          const treatment = condition.treatment;
-  
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-  
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-        } else {
-          resultDiv.innerHTML = "Condition not found.";
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        resultDiv.innerHTML = "An error occurred while fetching data.";
-      });
-  }
-  
-  btnSearch.addEventListener("click", searchCondition);
-  
+  fetch("travel_reccommendation_api.json")
+    .then((response) => response.json())
+    .then((data) => {
+
+      const beaches = [...data.beaches];
+      const temples = [...data.temples];
+      const countries = [...data.countries[1].cities];
+
+      if (input === "beaches" || input === "beach") {
+        destination = beaches;
+      } else if (input === "temples" || input === "temple") {
+        destination = temples;
+      } else if (input === "countries" || input === "country") {
+        destination = countries;
+      }
+
+      if (destination) {
+        destination.map((item) => {
+          console.log(typeof destination.toString());
+          resultDiv.style.display = "block";
+
+          const name = item.name;
+          const image = item.imageUrl;
+          const description = item.description;
+
+          resultDiv.innerHTML += `<img class='resultImage' src="${image}" alt="hjh">`;
+          resultDiv.innerHTML += `<h2 class='resultName' >${name}</h2>`;
+          resultDiv.innerHTML += `<p class='resultDescription' > ${description}</p>`;
+          resultDiv.innerHTML += `<div class="textContainer_btn resultBtn">
+                  <button>Book Now</button>
+                </div>`;
+        });
+      } else {
+        resultDiv.style.display = "block";
+        resultDiv.innerHTML = "Destination not found.";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      resultDiv.innerHTML = "An error occurred while fetching data.";
+    });
+}
+
+function clearSearch() {
+  const resultDiv = document.getElementById("result");
+  document.getElementById("conditionDestination").value = "";
+  resultDiv.style.display = "none";
+}
+
+btnSearch.addEventListener("click", searchLocation);
+btnReset.addEventListener("click", clearSearch);
